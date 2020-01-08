e6aa41689930df4d3dcc14161fedb01fcc7392d2
Kevin Petermann <kevin.petermann@fielmann.com>
2020-01-08 13:17:33 +0100

    [squareClick] Spielerabfrage und Array für Tabellenzellen

--- src/Tic-Tac-Toe.js
+++ src/Tic-Tac-Toe.js
@@ -1,34 +1,62 @@
-var currentPlayerName = 'X'; /*Aktuelle Spieler*/
-$('span[name="whoseturn"]').text(currentPlayerName); /*Nächster Zug*/
-$('.player[name="X"]').addClass('highlight'); /*Aktuellen Spieler hervorheben*/
-$('.player[name="O"]').addClass('unhighlight'); 
+//Variable wenn das Spiel vorbei ist, zu false initialisieren 
+var gameOver = false;
 
-$('td').click( /*Klick auf Tabelle*/
-	function() {
-		if(currentPlayerName == 'X') { /*Wenn Spieler X am Zug*/
-			$(this).text('X');
-			$('span[name="whoseturn"]').text('O'); /*Nächsten Spieler anzeigen*/
+// Abfrage, welcher Spieler beginnnen soll (if)
+if(confirm("Soll X starten?") == true) {
+	var player = 'X';
+} else {
+	var playere = 'O';
+}
 
-			$('.player[name="X"]').removeClass('highlight'); /*Button X highlighten*/
-			$('.player[name="X"]').addClass('unhighlight');
+// Array für jedes Zelle der Tabelle
+var squares = new Array();
+squares[0] = 0;
+squares[1] = 1;
+squares[2] = 2;
+squares[3] = 3;
+squares[4] = 4;
+squares[5] = 5;
+squares[6] = 6;
+squares[7] = 7;
+squares[8] = 8;
 
-			$('.player[name="O"]').removeClass('unhighlight'); /*Button O highlighten*/
-			$('.player[name="O"]').addClass('highlight');
+// Gewinnkombinationen
+var winCombinations = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
 
-			currentPlayerName = 'O'; /*Spieler O am Zug*/
-				
-		} else { /*Gegensätzlich zu oben*/
+// Funktion squareClick, Parameter square
+function squareClick(Square) {
+	
+	// Variable, welche die ID des Square enthält
+	var idElement = document.getElementById(square).value;
+	
+	// Variable, ParseInt als Argument übergeben und 1 vom Ergebnis des Aufrufs subtrahieren
+	// Gibt den Index des Arrays von Square aus, auf den der User klickt
+	var parseSquare = ((parseInt(square.substring(3, 4))) -1);
 
-			$(this).text('O');
-			$('span[name="whoseturn"]').text('X');
+	// Wenn die Variable idElement leer ist 
+	if(idElement == "") {
+		// Mithilfe der ID des Parameters square das HTML-Element updaten
+		// Inhalt der Variable player angleichen
+		document.getElementById(square).value = player;
 
-			$('.player[name="O"]').removeClass('highlight');
-			$('.player[name="O"]').addClass('unhighlight');
-
-			$('.player[name="X"]').removeClass('unhighlight');
-			$('.player[name="X"]').addClass('highlight');
-
-			currentPlayerName = 'X';		
-		}
+		//
+		squares[parseSquare] = player;
 	}
-);
+
+	// Funktion checkForWinner X
+
+
+	// Funktion CheckForWinner O
+
+
+	// Spieler tauschen
+}
